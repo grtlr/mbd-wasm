e45eeb22681c3ea6a32050b5fbb3e3fa9fe11d76
Jochen Görtler <web@goertler.email>
2021-04-26 09:46:46 +0200

    :bookmark: Fix exports and bump version

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
 import rust from "@wasm-tool/rollup-plugin-rust";
 import { terser } from 'rollup-plugin-terser';
 
+import pkg from './package.json'
+const copyright = `// ${pkg.homepage} v${pkg.version} Copyright ${(new Date).getFullYear()} ${pkg.author.name}`;
+
 export default [
     {
-        input: {
-            "mbd-wasm": "Cargo.toml",
-        },
+        input: 'index.js',
         output: {
-            dir: "dist/",
+            file: "dist/mbd-wasm.js",
             format: "umd",
             sourcemap: true,
             name: "mbd-wasm",
@@ -17,14 +18,13 @@ export default [
         ],
     },
     {
-        input: {
-            "mbd-wasm.cjs": "Cargo.toml",
-        },
+        input: 'index.js',
         output: {
-            dir: "dist/",
+            file: "dist/mbd-wasm.cjs.js",
             format: "cjs",
             sourcemap: true,
             name: "mbd-wasm.cjs",
+            exports: 'default',
         },
         plugins: [
             rust({
@@ -33,20 +33,16 @@ export default [
         ],
     },
     {
-        input: {
-            "mbd-wasm.min": "Cargo.toml",
-        },
+        input: 'index.js',
         output: {
-            dir: "dist/",
+            file: "dist/mbd-wasm.min.js",
             format: "umd",
             sourcemap: true,
             name: "mbd-wasm.min",
         },
         plugins: [
-            rust({
-                inlineWasm: true,
-            }),
-            terser(),
+            rust(),
+            terser({output: {preamble: copyright}}),
         ],
     }
 ];
